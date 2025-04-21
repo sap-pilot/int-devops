dd32cbc8ec8866b2644cfc5659d3498ae35bf2b8
Da Chen <da.chen@me.com>
2025-04-20 20:41:32 -0700

    Add upload proxy and test scripts

--- srv/server.js
+++ srv/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
 
 const { logger } = require("./common/logger");
-let buildInfo = {"version":"N/A","built":"N/A"};
+let buildInfo = { "version": "N/A", "built": "N/A" };
 try { buildInfo = require("./build-info.json") } catch (e) { logger.warn(`build-info.json not found`) }
 
 const app = express();
@@ -10,9 +10,11 @@ const port = process.env.PORT || 4004;
 
 /* http-proxy middleware */
 const tmsProxyConfig = {
-    target: 'https://services.odata.org', // target to be determined by destination via router function
+    // target:  'https://services.odata.org',
+    // pathRewrite: { '^/': '/northwind/northwind.svc/' },
+    target: 'https://transport-service-app-backend.ts.cfapps.us10.hana.ondemand.com', // target to be determined by destination via router function
     changeOrigin: true,
-    pathRewrite: { '^/': '/northwind/northwind.svc/' },
+    selfHandleResponse: true, // res.end() will be called internally by responseInterceptor()
     // router: (req) => {
     //     const destination = req.destination;
     //     return destination.url;
@@ -20,16 +22,31 @@ const tmsProxyConfig = {
     on: {
         proxyReq: (proxyReq, req, res) => {
             req.startTime = Date.now();
+            const exchange = `[request] ${req.method} ${req.path}`;
+            logger.info(exchange); // GET / -> http://www.example.com [200]
         },
-        proxyRes: (proxyRes, req, res) => {
+        // proxyRes: (proxyRes, req, res) => {
+        //     const durationMs = Date.now() - req.startTime;
+        //     logger.info(`tms request url: ${req.url}, response code: ${proxyRes.statusCode}, duration(ms): ${durationMs}`);
+        //     if (proxyRes.statusCode >= 500) {
+        //         logger.error(`tms response code: ${proxyRes.statusCode}, request url: ${req.url}, req-headers: ${JSON.stringify(req.headers)}, res-headers: ${JSON.stringify(res.getHeaders())}`);
+        //     } else if (proxyRes.statusCode >= 300) {
+        //         logger.warn(`tms response code: ${proxyRes.statusCode}, request url: ${req.url}, req-headers: ${JSON.stringify(req.headers)}, res-headers: ${JSON.stringify(res.getHeaders())}`);
+        //     }
+        // },
+        proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+            // log original request and proxied request info
             const durationMs = Date.now() - req.startTime;
-            logger.info(`tms request url: ${req.url}, response code: ${proxyRes.statusCode}, duration(ms): ${durationMs}`);
-            if (proxyRes.statusCode >= 500) {
-                logger.error(`tms response code: ${proxyRes.statusCode}, request url: ${req.url}, req-headers: ${JSON.stringify(req.headers)}, res-headers: ${JSON.stringify(res.getHeaders())}`);
-            } else if (proxyRes.statusCode >= 300) {
-                logger.warn(`tms response code: ${proxyRes.statusCode}, request url: ${req.url}, req-headers: ${JSON.stringify(req.headers)}, res-headers: ${JSON.stringify(res.getHeaders())}`);
-            }
-        },
+            const exchange = `[response] ${req.method} ${req.path} -> ${proxyRes.req.protocol}//${proxyRes.req.host}${proxyRes.req.path} [${proxyRes.statusCode}] (${durationMs}ms)`;
+            logger.info(exchange); // GET / -> http://www.example.com [200]
+            //logger.warn(`[headers] req-headers: ${JSON.stringify(req.headers)}, res-headers: ${JSON.stringify(res.getHeaders())}`);
+
+            // log complete response
+            const response = responseBuffer.toString('utf8');
+            logger.debug(response); // log response body
+
+            return responseBuffer;
+        }),
         error: (err, req, res) => {
             logger.error(err);
         }
